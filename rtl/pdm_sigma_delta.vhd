577eda3a3e7d09ceded1e3eaa123f6a21fe99ea0
Sean Pierce <seandpierce@gmail.com>
2022-03-17 09:17:49 +1300

    basic reproduction (lowres) working
    Signed-off-by: Sean Pierce <seandpierce@gmail.com>

--- rtl/pdm_sigma_delta.vhd
+++ rtl/pdm_sigma_delta.vhd
@@ -26,7 +26,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity pdm_sigma_delta is
     Port ( i_clk, i_pdm: in STD_LOGIC;
            i_sine_wave: in STD_LOGIC_VECTOR(23 downto 0);
-           o_waveform: out STD_LOGIC_VECTOR(23 downto 0));
+           o_waveform: out SIGNED(23 downto 0));
 
 end pdm_sigma_delta;
 
@@ -34,17 +34,25 @@ architecture Behavioral of pdm_sigma_delta is
 
 begin
     process_clock : PROCESS (i_clk)
-        variable memory : UNSIGNED(63 downto 0) := (others => '0');
-        variable pdm : UNSIGNED(63 downto 0) := (others => '0');
+        variable memory : UNSIGNED(23 downto 0) := (others => '0');
+        variable sum : SIGNED(23 downto 0) := (others => '0');
+
     begin
         if (i_clk'event and i_clk = '1') then
-            --if (i_pdm = '1') then
-            --    pdm := '1';
-            --else
-            --    pdm := '0';
-            --end if;
+            
+            memory := shift_left(memory,1);
+            memory(0) := i_pdm;
+            sum := (others => '0');
 
-            memory := shift_left(unsigned(memory),1);-- + pdm;
+            for i in 0 to 23 loop
+                if memory(i) = '1' then
+                    sum := sum + 1;
+                else
+                    sum := sum -1;
+                end if;
+            end loop;    
+
+            o_waveform <= sum;
         end if;
     end process;
 
