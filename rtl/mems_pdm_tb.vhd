c76764ad815fa03f262c42efa3620789bdc94e92
stevieraysean <seandpierce@gmail.com>
2022-03-31 16:10:17 +1300

    MEMs PDM Testbench - Tidy up, remove unused vars
    Signed-off-by: stevieraysean <seandpierce@gmail.com>

--- rtl/mems_pdm_tb.vhd
+++ rtl/mems_pdm_tb.vhd
@@ -18,14 +18,11 @@ architecture Behavioral of mems_pdm_tb is
     constant c_CLOCK_FREQ_HZ : real := 3072000.0; -- 3.072 MHz
     constant c_CLOCK_PERIOD : real := (1.0 / c_CLOCK_FREQ_HZ);
     constant c_CLOCK_PERIOD_HALF : time := (c_CLOCK_PERIOD / 2) * 1 sec;
-
     constant c_SIM_BIT_DEPTH : integer := 24;
 
     signal r_Clock : std_logic := '0';
     signal r_adc : STD_LOGIC := '0';
     signal r_sine_wave : STD_LOGIC_VECTOR((c_SIM_BIT_DEPTH-1) downto 0) := (others => '0');
-    signal s_signal_sign : STD_LOGIC := '0';
-
 
 begin        
     
@@ -34,10 +31,8 @@ begin
     sine_wave : process(r_Clock)
         variable v_tstep : real := 0.0;
         variable v_analog_sig : real := 0.0;
-        variable v_amp : real := 0.95;
+        variable v_amp : real := 0.95; -- prevent clipping, TODO: fix & remove
         variable v_analog_sig_sign : INTEGER := 1;
-        variable v_period_modifier : INTEGER := 1;
-
         variable v_difference : real := 0.0;
         variable v_integrator : real := 0.0;
         variable v_dac : real := 0.0;
@@ -54,7 +49,7 @@ begin
                 c_SINE_FREQ_HZ := c_SINE_FREQ_HZ + 0.05;
             end if;
 
-            v_analog_sig := v_amp * sin(MATH_2_PI * v_tstep * c_SINE_FREQ_HZ * v_period_modifier);
+            v_analog_sig := v_amp * sin(MATH_2_PI * v_tstep * c_SINE_FREQ_HZ);
 
             v_difference := v_analog_sig - v_dac;
             v_integrator := v_difference + v_integrator;
@@ -67,7 +62,8 @@ begin
                 v_dac := -1.0;
             end if;
 
-            r_sine_wave <= STD_LOGIC_VECTOR(to_signed(INTEGER(v_analog_sig*((2 ** (c_SIM_BIT_DEPTH-1)) -1)),c_SIM_BIT_DEPTH));
+            -- scale up to desired bit-depth for audio
+            r_sine_wave <= STD_LOGIC_VECTOR(to_signed(INTEGER(v_analog_sig*((2 ** (c_SIM_BIT_DEPTH-1)) -1)), c_SIM_BIT_DEPTH));
         end if;
     end process;
     
