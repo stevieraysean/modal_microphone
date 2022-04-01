2f15e1d1a00a87106f6c05feebccd50ec0e23fc1
stevieraysean <seandpierce@gmail.com>
2022-04-01 17:50:27 +1300

    FIR Filter: - Filter working - Added TestBench for Filter - needs better attenuation (calc coeffs)
    Signed-off-by: stevieraysean <seandpierce@gmail.com>

--- rtl/fir_filter.vhd
+++ rtl/fir_filter.vhd
@@ -29,7 +29,8 @@ use work.data_types.ALL;
 entity fir_filter is
     generic (
         g_BITDEPTH           : integer := 24;
-        g_COEFFICIENTS       : array_of_integers := ( 11, -7, -33, 113, 255, 113, -33,  -7,  11);
+        --g_COEFFICIENTS       : array_of_integers := ( 11, -7, -33, 113, 255, 113, -33,  -7,  11); -- TODO: half-band w/  zeros 
+        g_COEFFICIENTS       : array_of_integers := (176, 0, -299, 0, 1023,  817, 1023, 0, -299, 0, 176);
         g_DECIMATION_RATE    : integer := 2
         );
     Port ( 
@@ -39,8 +40,6 @@ entity fir_filter is
         );
 
     constant g_STAGES : integer := g_COEFFICIENTS'LENGTH-1;
-    -- constant g_STAGES : integer := ;
-
 end fir_filter;
 
 architecture Behavioral of fir_filter is
@@ -52,10 +51,8 @@ architecture Behavioral of fir_filter is
     signal r_mults   : t_mult_stage := (others => to_signed(0, g_BITDEPTH*2));
     signal r_sums    : t_mult_stage := (others => to_signed(0, g_BITDEPTH*2));
 
-    signal r_mult1   : signed((g_BITDEPTH*2)-1 downto 0) := (others => '0');
-
 begin
-    -- fir delay taps 
+    -- delays
     process_integrator : PROCESS (i_clk)
     begin
         if (i_clk'event and i_clk = '1') then
@@ -66,17 +63,19 @@ begin
                     r_taps(STAGE) <= r_taps(STAGE-1);
                 end if;
             end loop;
-            r_mult1 <= (signed(i_SIGNAL_IN) * g_COEFFICIENTS(0));
         end if;
     end process;
 
-    
-    -- multiplies and sums
-    g_GENERATE_sum : for STAGE in 0 to g_STAGES-1 generate
+    -- multiplies
+    g_GENERATE_mult : for STAGE in 0 to g_STAGES-1 generate
         r_mults(STAGE)  <= r_taps(STAGE) * g_COEFFICIENTS(STAGE+1);
-        r_sums(STAGE)  <= r_mult1 + r_mults(STAGE) when STAGE = 0;
-        r_sums(STAGE)  <= r_sums(STAGE-1) + r_mults(STAGE) when STAGE >= 1;
+    end generate g_GENERATE_mult;
+
+    -- sums
+    r_sums(0)  <= (signed(i_SIGNAL_IN) * g_COEFFICIENTS(0)) + r_mults(0);
+    g_GENERATE_sum : for STAGE in 1 to g_STAGES-1 generate
+        r_sums(STAGE)  <= r_sums(STAGE-1) + r_mults(STAGE);
     end generate g_GENERATE_sum;
 
-    o_SIGNAL_OUT <= STD_LOGIC_VECTOR(r_sums(g_STAGES-1)(g_BITDEPTH-1 downto 0));
+    o_SIGNAL_OUT <= STD_LOGIC_VECTOR(r_sums(g_STAGES-1)(35 downto 12));
 end Behavioral;
